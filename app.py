97e2e7ce4950adc8f12c4b3e4f3e3e31acf6291c
pranav3016 <pranavchowdhary10@gmail.com>
2024-04-18 00:17:07 -0400

    message

--- app.py
+++ app.py
@@ -1,19 +1,1 @@
-<<<<<<< HEAD
-#comments to check diff
-=======
-import streamlit as st
-from PyPDF2 import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-import os
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
-import google.generativeai as genai
-from langchain.vectorstores import FAISS
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.chains.question_answering import load_qa_chain
-from langchain.prompts import PromptTemplate
-from dotenv import load_dotenv
 
-load_dotenv()
-os.getenv("GOOGLE_API_KEY")
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
->>>>>>> 07a81051802b7a5b667504e579d47c23b620408f
